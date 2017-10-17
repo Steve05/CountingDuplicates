708a556f9b544359dc4ad72b7ba5b25faa6a67e1
Steve05 <steve.yeh05@gmail.com>
2017-10-17 10:39:55 +0800

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CountingDuplicates")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CountingDuplicates")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42ed9a35-da81-48f8-b775-fd81c6905756")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CountingDuplicates
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void input_empty_should_return_zero()
+        {
+            AssertDuplicateCountShouldBe(0, "");
+        }
+
+        [TestMethod]
+        public void input_abcde_should_return_zero()
+        {
+            AssertDuplicateCountShouldBe(0, "abcde");
+        }
+
+        [TestMethod]
+        public void input_aabbcde_should_return_two()
+        {
+            AssertDuplicateCountShouldBe(2, "aabBcde");
+        }
+
+        [TestMethod]
+        public void input_aabBcde_should_return_two()
+        {
+            AssertDuplicateCountShouldBe(2, "aabBcde");
+        }
+
+        [TestMethod]
+        public void input_Indivisibility_should_return_one()
+        {
+            AssertDuplicateCountShouldBe(1, "Indivisibility");
+        }
+
+        [TestMethod]
+        public void input_Indivisibilities_should_return_two()
+        {
+            AssertDuplicateCountShouldBe(2, "Indivisibilities");
+        }
+
+        private static void AssertDuplicateCountShouldBe(int expected, string str)
+        {
+            var kata = new Kata();
+            var actual = kata.DuplicateCount(str);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int DuplicateCount(string str)
+        {
+            return string.IsNullOrEmpty(str) ? 0 : str.ToLower().GroupBy(x => x).Count(@group => @group.Count() >= 2);
+        }
+
+    }
+}
+
+   
+
+
